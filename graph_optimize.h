0c0292c4b72e700f65275994d6f10650fd99f32c
daofeng <9779716+du-daofeng@user.noreply.gitee.com>
2022-05-13 18:02:44 +0800

    pose graph optimize

--- graph_optimize.h
+++ graph_optimize.h
@@ -5,6 +5,8 @@
 #include <Eigen/Dense>
 #include <cmath>
 
+#include <Eigen/Sparse>
+#include <Eigen/Eigen>
 
 namespace graph
 {
@@ -19,33 +21,102 @@ public:
 	using Vector2 = typename Eigen::Matrix<DataType, 2, 1>;
 		
 
-	GraphOptimize()
-	{
+	GraphOptimize() = delete;
 
-	}
+	explicit GraphOptimize( const int vertex_num )
+	{
+		H.resize( vertex_num * 3, vertex_num * 3 );
+		b.resize( vertex_num * 3 );
+	} 
 
 	~GraphOptimize()
 	{
 
 	}
 
+	void setVertexNumber( const int vertex_num )
+	{
+		H.resize( vertex_num * 3, vertex_num * 3 );
+		b.resize( vertex_num * 3 );
+	}
+
 	void getHessianDerived( const std::vector<Vector3> &v_poses,
 				const std::vector<int> &from_ids,
 				const std::vector<int> &to_ids,
-				const std::vector<Vector3> &e_means )
+				const std::vector<Vector3> &e_means,
+				const Matrix3x3 &info_matrix )
 	{
+		//Eigen::SparseMatrix<DataType> H( v_poses.size() * 3, v_poses.size() * 3 );
+		b_i = Vector3::Zero();
+		b_j = Vector3::Zero();
+		H_ii = Matrix3x3::Zero();
+		H_ij = Matrix3x3::Zero();
+		H_ji = Matrix3x3::Zero();
+		H_jj = Matrix3x3::Zero();		
 
+		for( size_t i = 0; i < e_means.size(); i ++ ){
+			int id_i = from_ids[i];
+			int id_j = to_ids[i];
+
+			linearFactors( v_poses, from_ids, to_ids, e_means, i );
+			
+			b_i = -A.transpose() * info_matrix * e;
+			b_j = -B.transpose() * info_matrix * e;
+			H_ii = A.transpose() * info_matrix * A;
+			H_ij = A.transpose() * info_matrix * B;
+			H_ji = B.transpose() * info_matrix * A;
+			H_jj = B.transpose() * info_matrix * B;
+
+			std::vector <Eigen::Triplet<DataType>> triplets;
+			for( int i = 0; i < H_ii.rows(); i ++ ){
+				for( int j = 0; j < H_ii.cols(); j ++ ){
+					triplets.emplace_back( id_i * 3 + i, id_i * 3 + j, H_ii( i, j ) );			
+				}
+			}
+			
+			for( int i = 0; i < H_ij.rows(); i ++ ){
+				for( int j = 0; j < H_ij.cols(); j ++ ){
+					triplets.emplace_back( id_i * 3 + i, id_j * 3 + j, H_ij( i, j ) );
+				}
+			}
+			
+			for( int i = 0; i < H_ji.rows(); i ++ ){
+				for( int j = 0; j < H_ji.cols(); j ++ ){
+					triplets.emplace_back( id_j * 3 + i, id_i * 3 + j, H_ji( i, j ) );
+				}
+			}
+
+			for( int i = 0; i < H_jj.rows(); i ++ ){
+				for( int j = 0; j < H_jj.rows(); j ++ ){
+					triplets.push_back( id_j * 3 + i, id_j * 3 + j, H_jj( i, j ) );
+				}
+			}
+			
+			H.setFromTriplets( triplets.begin(), triplets.end() );
+
+			triplets.clear();
+			for( int i = 0; i < b_i.size(); i ++ ){
+				triplets.emplace_back( id_i * 3 + i, 0, b_i( i ) );	
+			}
+	
+			for( int i = 0; i < b_j.size(); i ++ ){
+				triplets.emplace_back( id_j * 3 + i, 0, b_j( i ) );
+			}
+	
+			b.setFromTriplets( triplets.begin(), triplets.end() );
+		}
 	}
 
 	void linearFactors( const std::vector<Vector3> &v_poses, 
 			    const std::vector<int> &from_ids,
 			    const std::vector<int> &to_ids,
 			    const std::vector<Vector3> &e_means,
-			    const int index,
-			    Matrix3x3 &A,
-			    Matrix3x3 &B,	
-			    Vector3 &e)
+			    const int index )
 	{
+		A = Matrix3x3::Zero();
+		B = Matrix3x3::Zero();
+		e = Vector3::Zero();
+	
 		int id_i = from_ids[index];
 		int id_j = to_ids[index];
 		
@@ -107,6 +178,21 @@ private:
 	
 		return v;
 	}
+
+private:
+	Eigen::SparseMatrix<DataType> H;	
+	Eigen::SparseVector<DataType> b;
+
+	Matrix3x3 A;
+	Matrix3x3 B;
+	Vector3 e;
+
+	Vector3 b_i;
+	Vector3 b_j;
+	Matrix3x3 H_ii;
+	Matrix3x3 H_ij;
+	Matrix3x3 H_ji;
+	Matrix3x3 H_jj; 
 };
 
 }
